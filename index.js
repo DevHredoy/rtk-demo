3370b29c3f75b2c15d6598942a370086b709f9e0
DevHredoy <rafiul.hredoy@technonext.com>
2025-01-12 20:31:52 +0600

    ..

---
+++ index.js
@@ -0,0 +1,16 @@
+const store = require("./app/store");
+
+const cakeActions = require("./app/features/cake/cakeSlice").cakeActions;
+
+console.log("Initial state", store.getState());
+
+const unsubscribe = store.subscribe(() => {
+  console.log("updated state ", store.getState());
+});
+
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+
+unsubscribe();
